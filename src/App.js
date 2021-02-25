e70865296b4d251f18f07abc34b238ccfc6ba6ff
zhangteng <1820057934@qq.com>
2021-02-25 15:04:39 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
+import ReduxDemo from './pages/reduxDemo/ReduxDemo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavLink to='/redux'>Redux示例</NavLink>
+      <Redirect to = '/' />
+
+      <Switch>
+        <Route path='/redux' component={ReduxDemo} />
+      </Switch>
+    </>
   );
 }
 
